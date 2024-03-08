ab063df83f1532ff4c0c5097c9fca5b6a89b2ef7
NataliCab <161525089+NataliCab@users.noreply.github.com>
2024-03-07 23:31:33 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const toggle=document.querySelector(".toggle")
+const navMenu=document.querySelector(".nav-menu");
+toggle.addEventListener("click",()=>{ 
+    navMenu.classList.toggle("nav-menvi");});
